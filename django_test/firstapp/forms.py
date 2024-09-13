2f665cb894db968d68bf45c68e9085ad96c565c1
EfanovEvgeny1 <eugenye@bk.ru>
2024-09-13 13:51:48 +0300

    13.09.24 Ефанов Дубровина цуканов ЛЗ 9

--- django_test/firstapp/forms.py
+++ django_test/firstapp/forms.py
@@ -1,28 +1,7 @@
 from django import forms
 
 class UserForm(forms.Form):
-    """
-    name = forms.CharField(label="Имя клиента", max_length=15, help_text="ФИO не более 15 символов")
-    age = forms.IntegerField(label="Возраст клиента")
-    email = forms.EmailField(label="Электронный адрес", help_text="Обязательный символ - @")
-    basket = forms.BooleanField(label="Положить товар в корзину", required=False)
-    vyb = forms.NullBooleanField(label="Вы поедете в Сочи этим летом?")
-    ip_adres = forms.GenericIPAddressField(label="IP адрес", help_text=" Пример формата 192.0.2.0")
-    reg_text = forms.RegexField(label="Текст", regex="^[0-9][A-F][0-9]$")
-    slug_text = forms.SlugField(label="Введите текст")
-    url_text = forms.URLField(label="Введите URL", help_text="Например http://www.google.com")
-    uuid_text = forms.UUIDField(label="Введите UUID", help_text="Формат xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxx")
-    combo_text = forms.ComboField(label="Введите URL",
-                                  fields=[forms.URLField(), forms.CharField(max_length=20)])
-    file_path = forms.FilePathField(label="Выберите файл", path="D:/",
-                                    allow_files="True", allow_folders="True")
-    file = forms.ImageField(label="Изображение")
-    """
-    date = forms.DateField(label="Введите дату")
-
-    city = forms.TypedMultipleChoiceField(label="Выберите город",
-                                          empty_value=None, 
-                                          choices=((1, "Москва"), 
-                                                   (2, "Воронеж"),
-                                                   (3, "Курск"),
-                                                   (4, "Томск")))
+    name = forms.CharField(label="Имя клиента",
+    widget=forms.TextInput(attrs={"class": "myfield"}))
+    age = forms.IntegerField(label="Возраст клиента",
+    widget=forms.NumberInput(attrs={"class": "myfield"}))
--- django_test/firstapp/views.py
+++ django_test/firstapp/views.py
@@ -6,8 +6,18 @@ from django.template.response import TemplateResponse
 
 
 def index(request):
-    userform = UserForm()
-    return render(request, "firstapp/index.html", {"form": userform})
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            name = userform.cleaned_data["name"]
+            return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
+        else:
+            return HttpResponse("Ошибка ввода данных")
+    else:
+        userform = UserForm()
+        return render(request, "firstapp/index.html", {"form": userform})
+
+
 
 
 def products(request, productid):
